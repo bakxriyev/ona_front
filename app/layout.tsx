7ad82a1c166abd4a7ec86e124dc13611a920dbed
bakxriyev <kamronbekbahriyev18@gmail.com>
2026-02-21 14:51:27 +0500

    dsds

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,9 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { Inter, Playfair_Display } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import { Providers } from "../components/providers"
 import "./globals.css"
 
-const inter = Inter({ subsets: ["latin", "cyrillic"] })
-const playfair = Playfair_Display({
-  subsets: ["latin", "cyrillic"],
-  variable: "--font-serif",
-})
 
 export const metadata: Metadata = {
   title: "Ona-bola Hospital",
@@ -27,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="uz">
-      <body className={`${inter.className} ${playfair.variable} antialiased`}>
+      <body className={`antialiased`}>
         <Providers>{children}</Providers>
         <Analytics />
       </body>
